10c4a8e6d8105187b453ae5785ed3b808499da07
Casecreature36 <101048588+Casecreature36@users.noreply.github.com>
2023-04-05 20:26:53 +0100

    White Square

--- src/main/GamePanel.java
+++ src/main/GamePanel.java
@@ -1,14 +1,45 @@
 package main;
 
 import javax.swing.JPanel;
+import java.awt.Color;
 import java.awt.Dimension;
+import java.awt.Graphics;
+import java.awt.Graphics2D;
 
 import static main.ids.EngineConstants.*;
 
-public class GamePanel extends JPanel {
+public class GamePanel extends JPanel implements Runnable {
+
+    Thread gameThread;
+
     public GamePanel() {
         this.setPreferredSize(new Dimension(SCREEN_WIDTH, SCREEN_HEIGHT));
         this.setBackground(BACKGROUND);
         this.setDoubleBuffered(DOUBLE_BUFFERED);
+    }
+
+    public void startGameThread() {
+        gameThread = new Thread(this);
+        gameThread.start();
+    }
+
+    @Override
+    public void run() {
+        while (gameThread != null) {
+            update();
+            repaint();
+        }
+    }
+
+    public void update() {
+
+    }
+
+    public void paintComponent(Graphics g) {
+        super.paintComponent(g);
+        Graphics2D g2 = (Graphics2D)g;
+        g2.setColor(Color.WHITE);
+        g2.fillRect(100, 100, TILE_SIZE, TILE_SIZE);
+        g2.dispose();
     }
 }
